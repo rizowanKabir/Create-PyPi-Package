8fbb25f9deac95083debce819eb470fce6fff3a1
rizowanKabir <sohagrizowan@gmail.com>
2026-08-17 16:07:24 +0600

    new files added

---
+++ test.py
@@ -0,0 +1,9 @@
+from PyInception.logger import logger 
+from PyInception.custom_exception import InvalidURLException
+
+# logger.info("This is a test log message from test.py")
+
+try: 
+    raise InvalidURLException()
+except Exception as e:
+    logger.error(f"Caught an exception: {e}") 
